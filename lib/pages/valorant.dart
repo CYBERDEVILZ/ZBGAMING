dc877fe7f0c500fc46a66f1f3af21cfa5faba178
Pranav <pranavcosmos4@gmail.com>
2021-12-26 22:46:42 +0530

    valorant page added

--- lib/pages/valorant.dart
+++ lib/pages/valorant.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class CsgoTournaments extends StatelessWidget {
-  const CsgoTournaments({Key? key}) : super(key: key);
+class ValorantTournaments extends StatelessWidget {
+  const ValorantTournaments({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
