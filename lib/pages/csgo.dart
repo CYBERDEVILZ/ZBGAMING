7073233c56c8539530bb32041ec7a3446b042e28
Pranav <pranavcosmos4@gmail.com>
2021-12-26 22:33:34 +0530

    csgo page added

---
+++ lib/pages/csgo.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CsgoTournaments extends StatelessWidget {
+  const CsgoTournaments({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text("CS GO TOURNAMENTS WILL APPEAR HERE"));
+  }
+}
