b79f049ebb6e87eec663d64d9638568a29e642cc
Pranav <pranavcosmos4@gmail.com>
2022-01-09 13:02:33 +0530

    login page done

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -44,6 +44,7 @@ class _LoginState extends State<Login> {
       controller: email,
       keyboardType: TextInputType.emailAddress,
       textInputAction: TextInputAction.next,
+      style: const TextStyle(color: Colors.white),
       validator: (value) {
         if (email.text.isEmpty) {
           return "Email field cannot be empty";
@@ -53,9 +54,16 @@ class _LoginState extends State<Login> {
         }
       },
       decoration: const InputDecoration(
+        enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+        errorBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.cyan)),
+        errorStyle: TextStyle(color: Colors.cyan),
+        focusedErrorBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.cyan, width: 2)),
         border: OutlineInputBorder(),
-        prefixIcon: Icon(Icons.mail),
-        label: Text("Email Address"),
+        prefixIcon: Icon(Icons.mail, color: Colors.white),
+        label: Text(
+          "Email Address",
+          style: TextStyle(color: Colors.white),
+        ),
       ),
     );
 
@@ -64,6 +72,7 @@ class _LoginState extends State<Login> {
       controller: passwd,
       obscureText: true,
       textInputAction: TextInputAction.done,
+      style: const TextStyle(color: Colors.white),
       validator: (value) {
         if (passwd.text.isEmpty) {
           return "Password field cannot be empty";
@@ -73,90 +82,99 @@ class _LoginState extends State<Login> {
         }
       },
       decoration: const InputDecoration(
+        enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+        errorBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.cyan)),
+        focusedErrorBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.cyan, width: 2)),
+        errorStyle: TextStyle(color: Colors.cyan),
         border: OutlineInputBorder(),
-        prefixIcon: Icon(Icons.vpn_key),
-        label: Text("Password"),
+        prefixIcon: Icon(Icons.vpn_key, color: Colors.white),
+        label: Text(
+          "Password",
+          style: TextStyle(color: Colors.white),
+        ),
       ),
     );
 
     // --------------- Return is Here --------------- //
-    return Scaffold(
-        body: SingleChildScrollView(
-            child: Form(
-      key: _formKey,
-      child: Column(
-        children: [
-          // image for login
-          const Placeholder(),
+    return SafeArea(
+      child: Scaffold(
+          backgroundColor: const Color(0xff111111),
+          body: SingleChildScrollView(
+              child: Form(
+            key: _formKey,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  const SizedBox(height: 50),
 
-          const SizedBox(height: 20),
+                  // image for login
+                  SizedBox(
+                      height: 150,
+                      child: Image.asset(
+                        "assets/images/zglitch.gif",
+                        fit: BoxFit.contain,
+                      )),
 
-          const Text(
-            "Login To Fight",
-            style: TextStyle(),
-            textScaleFactor: 2,
-          ),
+                  const SizedBox(height: 20),
 
-          const SizedBox(height: 5),
+                  // Email Entry
+                  Padding(padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10), child: emailEntry),
 
-          const Text(
-            "The Most Competitive Gaming Platform Ever",
-            style: TextStyle(),
-            textScaleFactor: 0.9,
-          ),
+                  // Password Entry
+                  Padding(padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10), child: passwdEntry),
 
-          const SizedBox(height: 10),
+                  const SizedBox(height: 40),
 
-          // Email Entry
-          Padding(padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10), child: emailEntry),
-
-          // Password Entry
-          Padding(padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10), child: passwdEntry),
-
-          const SizedBox(height: 20),
-
-          // Login Button
-          ElevatedButton(
-            onPressed: () {
-              validate();
-            },
-            child: isLoading
-                ? const CircularProgressIndicator(color: Colors.white)
-                : const Text(
-                    "Login",
-                    style: TextStyle(fontSize: 15),
+                  // Login Button
+                  ElevatedButton(
+                    onPressed: () {
+                      validate();
+                    },
+                    child: isLoading
+                        ? const CircularProgressIndicator(color: Colors.black)
+                        : const Text(
+                            "Login",
+                            style: TextStyle(fontSize: 15, color: Colors.black),
+                          ),
+                    style: ButtonStyle(
+                        elevation: MaterialStateProperty.all(0),
+                        fixedSize: MaterialStateProperty.all(const Size(150, 50)),
+                        backgroundColor: MaterialStateProperty.all(Colors.cyan)),
                   ),
-            style: ButtonStyle(
-                elevation: MaterialStateProperty.all(0), fixedSize: MaterialStateProperty.all(const Size(150, 50))),
-          ),
 
-          const SizedBox(height: 10),
+                  const SizedBox(height: 30),
 
-          // forgot password feature
-          const Text("Forgot Password", style: TextStyle(color: Colors.blue)),
+                  // forgot password feature
+                  const Text("Forgot Password", style: TextStyle(color: Colors.cyan)),
 
-          const SizedBox(height: 5),
+                  const SizedBox(height: 5),
 
-          // signup feature
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text("Don't have an account? "),
-              GestureDetector(
-                // signup navigation
-                onTap: () {},
-                child: const Text(
-                  "Create one",
-                  style:
-                      TextStyle(fontWeight: FontWeight.bold, color: Colors.blue, decoration: TextDecoration.underline),
-                ),
-              )
-            ],
-          ),
+                  // signup feature
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        "Don't have an account? ",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      GestureDetector(
+                        // signup navigation
+                        onTap: () {},
+                        child: const Text(
+                          "Create one",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, color: Colors.cyan, decoration: TextDecoration.underline),
+                        ),
+                      )
+                    ],
+                  ),
 
-          const SizedBox(height: 20),
-        ],
-      ),
-    )));
+                  const SizedBox(height: 20),
+                ],
+              ),
+            ),
+          ))),
+    );
   }
 }
