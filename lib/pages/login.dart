4c5b631e2e25009b0edfcaef65f0c9e13504508f
Pranav <pranavcosmos4@gmail.com>
2022-01-09 14:01:58 +0530

    login page modification

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -44,7 +44,7 @@ class _LoginState extends State<Login> {
       controller: email,
       keyboardType: TextInputType.emailAddress,
       textInputAction: TextInputAction.next,
-      style: const TextStyle(color: Colors.white),
+      style: const TextStyle(color: Colors.black),
       validator: (value) {
         if (email.text.isEmpty) {
           return "Email field cannot be empty";
@@ -54,15 +54,10 @@ class _LoginState extends State<Login> {
         }
       },
       decoration: const InputDecoration(
-        enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
-        errorBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.cyan)),
-        errorStyle: TextStyle(color: Colors.cyan),
-        focusedErrorBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.cyan, width: 2)),
         border: OutlineInputBorder(),
-        prefixIcon: Icon(Icons.mail, color: Colors.white),
+        prefixIcon: Icon(Icons.mail),
         label: Text(
           "Email Address",
-          style: TextStyle(color: Colors.white),
         ),
       ),
     );
@@ -72,7 +67,7 @@ class _LoginState extends State<Login> {
       controller: passwd,
       obscureText: true,
       textInputAction: TextInputAction.done,
-      style: const TextStyle(color: Colors.white),
+      style: const TextStyle(color: Colors.black),
       validator: (value) {
         if (passwd.text.isEmpty) {
           return "Password field cannot be empty";
@@ -82,15 +77,11 @@ class _LoginState extends State<Login> {
         }
       },
       decoration: const InputDecoration(
-        enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
-        errorBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.cyan)),
-        focusedErrorBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.cyan, width: 2)),
-        errorStyle: TextStyle(color: Colors.cyan),
         border: OutlineInputBorder(),
-        prefixIcon: Icon(Icons.vpn_key, color: Colors.white),
+        prefixIcon: Icon(Icons.vpn_key),
         label: Text(
           "Password",
-          style: TextStyle(color: Colors.white),
+          style: TextStyle(),
         ),
       ),
     );
@@ -98,81 +89,95 @@ class _LoginState extends State<Login> {
     // --------------- Return is Here --------------- //
     return SafeArea(
       child: Scaffold(
-          backgroundColor: const Color(0xff111111),
+          backgroundColor: Colors.white,
           body: SingleChildScrollView(
               child: Form(
             key: _formKey,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                children: [
-                  const SizedBox(height: 50),
+            child: Column(
+              children: [
+                const SizedBox(height: 50),
 
-                  // image for login
+                // image for login
+                Stack(children: [
                   SizedBox(
                       height: 150,
                       child: Image.asset(
                         "assets/images/zglitch.gif",
                         fit: BoxFit.contain,
+                        color: Colors.blue,
                       )),
-
-                  const SizedBox(height: 20),
-
-                  // Email Entry
-                  Padding(padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10), child: emailEntry),
-
-                  // Password Entry
-                  Padding(padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10), child: passwdEntry),
-
-                  const SizedBox(height: 40),
-
-                  // Login Button
-                  ElevatedButton(
-                    onPressed: () {
-                      validate();
-                    },
-                    child: isLoading
-                        ? const CircularProgressIndicator(color: Colors.black)
-                        : const Text(
-                            "Login",
-                            style: TextStyle(fontSize: 15, color: Colors.black),
-                          ),
-                    style: ButtonStyle(
-                        elevation: MaterialStateProperty.all(0),
-                        fixedSize: MaterialStateProperty.all(const Size(150, 50)),
-                        backgroundColor: MaterialStateProperty.all(Colors.cyan)),
-                  ),
-
-                  const SizedBox(height: 30),
-
-                  // forgot password feature
-                  const Text("Forgot Password", style: TextStyle(color: Colors.cyan)),
-
-                  const SizedBox(height: 5),
-
-                  // signup feature
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Text(
-                        "Don't have an account? ",
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      GestureDetector(
-                        // signup navigation
-                        onTap: () {},
-                        child: const Text(
-                          "Create one",
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold, color: Colors.cyan, decoration: TextDecoration.underline),
+                  Positioned.fill(
+                      bottom: 10,
+                      child: Align(
+                        child: Text(
+                          "Got Skills? Get Rewarded.",
+                          style: TextStyle(color: Colors.blue[600], fontWeight: FontWeight.bold),
                         ),
-                      )
-                    ],
-                  ),
+                        alignment: Alignment.bottomCenter,
+                      )),
+                ]),
 
-                  const SizedBox(height: 20),
-                ],
-              ),
+                const SizedBox(height: 20),
+
+                // Email Entry
+                Padding(padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10), child: emailEntry),
+
+                // Password Entry
+                Padding(padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10), child: passwdEntry),
+
+                const SizedBox(height: 40),
+
+                // Login Button
+                ElevatedButton(
+                  onPressed: () {
+                    validate();
+                  },
+                  child: isLoading
+                      ? const CircularProgressIndicator(color: Colors.white)
+                      : const Text(
+                          "Login",
+                          style: TextStyle(fontSize: 15, color: Colors.white),
+                        ),
+                  style: ButtonStyle(
+                      elevation: MaterialStateProperty.all(0),
+                      fixedSize: MaterialStateProperty.all(const Size(150, 50)),
+                      backgroundColor: MaterialStateProperty.all(Colors.blue)),
+                ),
+
+                const SizedBox(height: 30),
+
+                // forgot password feature
+                const Text("Forgot Password", style: TextStyle(color: Colors.blue)),
+
+                const SizedBox(height: 5),
+
+                // signup feature
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      "Don't have an account? ",
+                      style: TextStyle(color: Colors.black),
+                    ),
+                    GestureDetector(
+                      // signup navigation
+                      onTap: () {},
+                      child: const Text(
+                        "Create one",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, color: Colors.blue, decoration: TextDecoration.underline),
+                      ),
+                    )
+                  ],
+                ),
+
+                const SizedBox(height: 20),
+
+                Align(
+                    child: Image.asset("assets/images/zbunker-app-banner.png"),
+                    alignment: Alignment.bottomCenter,
+                    heightFactor: 0.8)
+              ],
             ),
           ))),
     );
