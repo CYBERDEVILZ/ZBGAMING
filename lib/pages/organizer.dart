ce895d536df57754e63b2a15de16e92b0892a450
Pranav <pranavcosmos4@gmail.com>
2022-01-12 12:30:04 +0530

    organzier login basic skeletion

--- lib/pages/organizer.dart
+++ lib/pages/organizer.dart
@@ -1,17 +1,201 @@
 // THIS IS FOR TESTING PURPOSE ONLY! DONT ADD IT TO PRODUCTION ENVIRONMENT
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class Organizer extends StatelessWidget {
+// ignore: implementation_imports
+import 'package:provider/src/provider.dart';
+
+import 'package:zbgaming/model/usermodel.dart';
+import 'package:zbgaming/pages/organizer_login.dart';
+
+class Organizer extends StatefulWidget {
   const Organizer({Key? key}) : super(key: key);
 
   @override
+  State<Organizer> createState() => _OrganizerState();
+}
+
+class _OrganizerState extends State<Organizer> {
+  @override
+  void initState() {
+    super.initState();
+
+    // check for authentication
+    FirebaseAuth.instance.authStateChanges().listen((event) {
+      if (event?.uid == null) {
+        Navigator.pushAndRemoveUntil(
+            context, MaterialPageRoute(builder: (context) => const OrganizerLogin()), (route) => false);
+      }
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Dashboard"),
-        elevation: 0,
-        centerTitle: true,
+    // drawer
+    Drawer organizerDrawer = Drawer(
+        child: ListView(
+      children: [
+        DrawerHeader(
+            margin: const EdgeInsets.all(0),
+            padding: const EdgeInsets.all(0),
+            child: Stack(
+              children: [
+                // zbunker element straight
+                Container(
+                    color: Colors.white,
+                    height: 400,
+                    width: MediaQuery.of(context).size.width,
+                    child: Image.asset("assets/images/zbunker-app-banner.png", fit: BoxFit.fitWidth)),
+                SizedBox(
+                  height: 400,
+                  width: double.infinity,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 5),
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          !(context.watch<UserModel>().imageurl == null)
+                              ? Stack(
+                                  children: [
+                                    CircleAvatar(backgroundColor: Colors.cyan[700], radius: 45),
+                                    Positioned(
+                                      top: 5,
+                                      left: 5,
+                                      child: CircleAvatar(
+                                        backgroundColor: Colors.cyan[700],
+                                        child: const CircularProgressIndicator(
+                                          color: Colors.white,
+                                        ),
+                                        foregroundImage: NetworkImage(context.watch<UserModel>().imageurl!),
+                                        radius: 40,
+                                      ),
+                                    )
+                                  ],
+                                )
+                              : CircleAvatar(
+                                  child: const FittedBox(
+                                    fit: BoxFit.cover,
+                                    child: Icon(
+                                      Icons.account_circle,
+                                      color: Colors.white,
+                                      size: 100,
+                                    ),
+                                  ),
+                                  backgroundColor: Colors.cyan[700],
+                                  radius: 45),
+                          Container(
+                            padding: const EdgeInsets.symmetric(horizontal: 30),
+                            child: Text(
+                              context.watch<UserModel>().username == null
+                                  ? "null"
+                                  : context.watch<UserModel>().username!,
+                              style: const TextStyle(fontSize: 20),
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          )
+                        ]),
+                  ),
+                )
+              ],
+            )),
+        ListTile(
+          onTap: () {}, // leads to registered matches
+          leading: const Icon(Icons.flag, color: Colors.black),
+          trailing: const Icon(Icons.arrow_right, color: Colors.black),
+          title: const Text(
+            "Registered Matches",
+            style: TextStyle(color: Colors.black),
+          ),
+        ),
+        ListTile(
+          onTap: () {}, // leads to favorites
+          leading: const Icon(Icons.star, color: Colors.black),
+          trailing: const Icon(Icons.arrow_right, color: Colors.black),
+          title: const Text(
+            "Favorites",
+            style: TextStyle(color: Colors.black),
+          ),
+        ),
+        ListTile(
+          onTap: () {}, // leads to match history
+          leading: const Icon(Icons.history, color: Colors.black),
+          trailing: const Icon(Icons.arrow_right, color: Colors.black),
+          title: const Text(
+            "History",
+            style: TextStyle(color: Colors.black),
+          ),
+        ),
+        ListTile(
+          onTap: () {}, // leads to my account
+          leading: const Icon(Icons.account_circle, color: Colors.black),
+          trailing: const Icon(Icons.arrow_right, color: Colors.black),
+          title: const Text(
+            "My Account",
+            style: TextStyle(color: Colors.black),
+          ),
+        ),
+        ListTile(
+          onTap: () {}, // leads to Settings
+          leading: const Icon(Icons.settings, color: Colors.black),
+          trailing: const Icon(Icons.arrow_right, color: Colors.black),
+          title: const Text(
+            "Settings",
+            style: TextStyle(color: Colors.black),
+          ),
+        ),
+        const SizedBox(height: 15),
+        Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: OutlinedButton(
+              // sign out user
+              onPressed: () async {
+                context.read<UserModel>().signout();
+                await FirebaseAuth.instance.signOut();
+              },
+              child: const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  "Sign Out",
+                  style: TextStyle(fontSize: 17),
+                ),
+              ),
+              style: ButtonStyle(
+                  foregroundColor: MaterialStateProperty.all(Colors.red),
+                  overlayColor: MaterialStateProperty.all(Colors.red[100]),
+                  side: MaterialStateProperty.all(const BorderSide(color: Colors.red, width: 2))),
+            )),
+        Container(
+          margin: const EdgeInsets.only(top: 50),
+          child: const Placeholder(), // zbgaming (copyrighted thing)
+          height: 100,
+        )
+      ],
+    ));
+
+    // --------------- Return is Here --------------- //
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const FittedBox(child: Text("Your Upcoming Matches")),
+          elevation: 0,
+          centerTitle: true,
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.headset_mic),
+              // navigate to customer care
+              onPressed: () {},
+            )
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          // add matches page
+          onPressed: () {},
+          child: const Icon(Icons.add),
+        ),
+        drawer: organizerDrawer,
       ),
     );
   }
---
+++ lib/pages/organizer_login.dart
@@ -0,0 +1,205 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+// ignore: implementation_imports
+import 'package:provider/src/provider.dart';
+
+import 'package:zbgaming/model/usermodel.dart';
+import 'package:zbgaming/pages/organizer.dart';
+import 'package:zbgaming/utils/routes.dart';
+
+// Notes for me.
+// How to validate a user?
+// 1. create a form key
+// 2. check the validation for form key when user press submit button
+// 3. on successfull validation (defined in each validate property of fields), take action
+
+class OrganizerLogin extends StatefulWidget {
+  const OrganizerLogin({Key? key}) : super(key: key);
+
+  @override
+  State<OrganizerLogin> createState() => _OrganizerLoginState();
+}
+
+class _OrganizerLoginState extends State<OrganizerLogin> {
+  final _formKey = GlobalKey<FormState>();
+
+  bool isLoading = false;
+
+  // text controllers
+  final TextEditingController email = TextEditingController();
+  final TextEditingController passwd = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    // validation
+    void validate() async {
+      isLoading = true;
+      setState(() {});
+      if (_formKey.currentState!.validate()) {
+        FirebaseAuth auth = FirebaseAuth.instance;
+        await auth.signInWithEmailAndPassword(email: email.text, password: passwd.text).then((value) async {
+          var data = await FirebaseFirestore.instance.collection("userinfo").doc(auth.currentUser!.uid).get();
+          context.read<UserModel>().setuid(FirebaseAuth.instance.currentUser!.uid);
+          context.read<UserModel>().setusername(data["username"]);
+          context.read<UserModel>().setemail(data["email"]);
+          context.read<UserModel>().setimageurl(data["imageurl"]);
+          await Fluttertoast.showToast(
+              msg: "Login successful!", backgroundColor: Colors.blue[700], textColor: Colors.white);
+          await Future.delayed(const Duration(seconds: 1));
+          Navigator.pushAndRemoveUntil(
+              context, MaterialPageRoute(builder: (context) => const Organizer()), (route) => false);
+        }).catchError((onError) => /* Write code for error */ null);
+      }
+      isLoading = false;
+      setState(() {});
+    }
+
+    // Email Entry
+    TextFormField emailEntry = TextFormField(
+      controller: email,
+      keyboardType: TextInputType.emailAddress,
+      textInputAction: TextInputAction.next,
+      style: const TextStyle(color: Colors.black),
+      validator: (value) {
+        if (email.text.isEmpty) {
+          return "Email field cannot be empty";
+        }
+        if (!RegExp("^[a-zA-Z0-9.-]+@[a-zA-Z0-9.-]+[.][a-zA-Z0-9]+\$").hasMatch(email.text)) {
+          return "Invalid Email";
+        }
+      },
+      decoration: const InputDecoration(
+        border: OutlineInputBorder(),
+        prefixIcon: Icon(Icons.mail),
+        label: Text(
+          "Email Address",
+        ),
+      ),
+    );
+
+    // Password Entry
+    TextFormField passwdEntry = TextFormField(
+      controller: passwd,
+      obscureText: true,
+      textInputAction: TextInputAction.done,
+      style: const TextStyle(color: Colors.black),
+      validator: (value) {
+        if (passwd.text.isEmpty) {
+          return "Password field cannot be empty";
+        }
+        if (passwd.text.length < 6) {
+          return "Minimum 6 characters needed";
+        }
+      },
+      decoration: const InputDecoration(
+        border: OutlineInputBorder(),
+        prefixIcon: Icon(Icons.vpn_key),
+        label: Text(
+          "Password",
+          style: TextStyle(),
+        ),
+      ),
+    );
+
+    // --------------- Return is Here --------------- //
+    return Scaffold(
+        resizeToAvoidBottomInset: false,
+        bottomSheet: Container(
+            color: Colors.white,
+            height: 100,
+            width: MediaQuery.of(context).size.width,
+            child: Image.asset(
+              "assets/images/zbunker-app-banner.png",
+              fit: BoxFit.fitWidth,
+            )),
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+            child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              const SizedBox(height: 50),
+
+              // image for login
+              Stack(children: [
+                SizedBox(
+                    height: 150,
+                    child: Image.asset(
+                      "assets/images/zglitch.gif",
+                      fit: BoxFit.contain,
+                      color: Colors.blue[700],
+                    )),
+                const Positioned.fill(
+                    bottom: 10,
+                    child: Align(
+                      child: Text(
+                        "Organize Amazing Tournaments",
+                        style: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold),
+                      ),
+                      alignment: Alignment.bottomCenter,
+                    )),
+              ]),
+
+              const SizedBox(height: 20),
+
+              // Email Entry
+              Padding(padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10), child: emailEntry),
+
+              // Password Entry
+              Padding(padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10), child: passwdEntry),
+
+              const SizedBox(height: 40),
+
+              // Login Button
+              ElevatedButton(
+                onPressed: () {
+                  validate();
+                },
+                child: isLoading
+                    ? const CircularProgressIndicator(color: Colors.white)
+                    : const Text(
+                        "Login",
+                        style: TextStyle(fontSize: 15, color: Colors.white),
+                      ),
+                style: ButtonStyle(
+                    elevation: MaterialStateProperty.all(0),
+                    fixedSize: MaterialStateProperty.all(const Size(150, 50)),
+                    backgroundColor: MaterialStateProperty.all(Colors.blue)),
+              ),
+
+              const SizedBox(height: 30),
+
+              // forgot password feature
+              const Text("Forgot Password", style: TextStyle(color: Colors.blue)),
+
+              const SizedBox(height: 5),
+
+              // signup feature
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Don't have an account? ",
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  GestureDetector(
+                    // signup navigation
+                    onTap: () {
+                      Navigator.pushNamed(context, AppRoutes.signup);
+                    },
+                    child: const Text(
+                      "Create one",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, color: Colors.blue, decoration: TextDecoration.underline),
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
+        )));
+  }
+}
