2d96d5b600d5fbc40fe1b271ac261bf629037839
Pranav <pranavcosmos4@gmail.com>
2022-03-04 12:58:16 +0530

    added signup endpoint

--- backend/venv/server_files/init.py
+++ backend/venv/server_files/init.py
@@ -1,3 +1,7 @@
+from email.mime import image
+from flask import Flask
+from flask import request
+import json
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
@@ -7,3 +11,20 @@ cred = credentials.Certificate('zbgaming-v1-firebase-adminsdk-2ozhj-4f38e5fc3e.j
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
+
+app = Flask(__name__)
+
+
+# USER SIGN UP
+@app.route("/user_signup")
+def home():
+  username = request.args.get('username')
+  email = request.args.get('email')
+  imageurl = None
+
+  if (username != None and email != None):
+    db.collection("userinfo").document().set({"username": username, "email": email, "imageurl": imageurl})
+
+  return "Will add futures to it. Right now it is static :("
+
+app.run(debug=True)
