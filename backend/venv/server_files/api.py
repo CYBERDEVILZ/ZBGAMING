280a6f2fbd1806a8e1862db3eca24bb312bb69dd
Pranav <pranavcosmos4@gmail.com>
2022-03-09 00:35:42 +0530

    added routes for csgo, freefire and valo registration

--- backend/venv/server_files/api.py
+++ backend/venv/server_files/api.py
@@ -109,6 +109,171 @@ def register():
   return "Failed"
 
 
+# REGISTER CSGO (DISABLE FOR NOW!!!!!!!!!!!!!!!!!!)
+@app.route("/api/register/csgo")
+def register():
+  matchuid = request.args.get("matchuid") 
+  useruid = request.args.get("useruid")
+  
+  # perform some validation here.......
+  # create a separate table of users who are registered for that match
+
+  if (matchuid != None and useruid != None):
+
+    # checking for already registered..
+    user = db.collection("userinfo").document(useruid).collection("registered").get()
+    user = [user.id for user in user]
+    if matchuid in user:
+      return "Failed: Already registered"
+
+
+    ref = db.collection("csgo").document(matchuid)
+    ref_obj = ref.get().to_dict()
+    try:
+      date = ref_obj["date"]
+      matchType = "csgo"
+      uid = matchuid
+      name = ref_obj["name"]
+    except:
+      return "Failure, no such doc"
+
+    # retrieve the total registered and update the value to one [USE TRANSACTION!]
+    transaction = db.transaction()
+    @firestore.transactional
+    def updateRegisteredTeams(transaction, ref):
+      snapshot = ref.get(transaction = transaction)
+      reg = snapshot.get("reg")
+      try:
+        if reg < 10:
+          transaction.update(ref, {"reg": reg + 1})
+          return True
+        else:
+          return False
+      except:
+        return False
+
+    
+    result = updateRegisteredTeams(transaction, ref)
+
+    if result:
+      db.collection("userinfo").document(useruid).collection("registered").document(matchuid).set({"date": date, "matchType": matchType, "name": name, "uid": uid})
+      return "Success"
+    else:
+      return "Failed"
+
+  return "Failed"
+
+
+# REGISTER VALORANT (DISABLE FOR NOW!!!!!!!!!!!!!!!!!!)
+@app.route("/api/register/valo")
+def register():
+  matchuid = request.args.get("matchuid") 
+  useruid = request.args.get("useruid")
+  
+  # perform some validation here.......
+  # create a separate table of users who are registered for that match
+
+  if (matchuid != None and useruid != None):
+
+    # checking for already registered..
+    user = db.collection("userinfo").document(useruid).collection("registered").get()
+    user = [user.id for user in user]
+    if matchuid in user:
+      return "Failed: Already registered"
+
+
+    ref = db.collection("valo").document(matchuid)
+    ref_obj = ref.get().to_dict()
+    try:
+      date = ref_obj["date"]
+      matchType = "valo"
+      uid = matchuid
+      name = ref_obj["name"]
+    except:
+      return "Failure, no such doc"
+
+    # retrieve the total registered and update the value to one [USE TRANSACTION!]
+    transaction = db.transaction()
+    @firestore.transactional
+    def updateRegisteredTeams(transaction, ref):
+      snapshot = ref.get(transaction = transaction)
+      reg = snapshot.get("reg")
+      try:
+        if reg < 10:
+          transaction.update(ref, {"reg": reg + 1})
+          return True
+        else:
+          return False
+      except:
+        return False
+
+    
+    result = updateRegisteredTeams(transaction, ref)
+
+    if result:
+      db.collection("userinfo").document(useruid).collection("registered").document(matchuid).set({"date": date, "matchType": matchType, "name": name, "uid": uid})
+      return "Success"
+    else:
+      return "Failed"
+
+  return "Failed"
+
+
+# REGISTER FREEFIRE (DISABLE FOR NOW!!!!!!!!!!!!!!!!!!)
+@app.route("/api/register/freefire")
+def register():
+  matchuid = request.args.get("matchuid") 
+  useruid = request.args.get("useruid")
+  
+  # perform some validation here.......
+  # create a separate table of users who are registered for that match
+
+  if (matchuid != None and useruid != None):
+
+    # checking for already registered..
+    user = db.collection("userinfo").document(useruid).collection("registered").get()
+    user = [user.id for user in user]
+    if matchuid in user:
+      return "Failed: Already registered"
+
+
+    ref = db.collection("freefire").document(matchuid)
+    ref_obj = ref.get().to_dict()
+    try:
+      date = ref_obj["date"]
+      matchType = "freefire"
+      uid = matchuid
+      name = ref_obj["name"]
+    except:
+      return "Failure, no such doc"
+
+    # retrieve the total registered and update the value to one [USE TRANSACTION!]
+    transaction = db.transaction()
+    @firestore.transactional
+    def updateRegisteredTeams(transaction, ref):
+      snapshot = ref.get(transaction = transaction)
+      reg = snapshot.get("reg")
+      try:
+        if reg < 50:
+          transaction.update(ref, {"reg": reg + 1})
+          return True
+        else:
+          return False
+      except:
+        return False
+
+    
+    result = updateRegisteredTeams(transaction, ref)
+
+    if result:
+      db.collection("userinfo").document(useruid).collection("registered").document(matchuid).set({"date": date, "matchType": matchType, "name": name, "uid": uid})
+      return "Success"
+    else:
+      return "Failed"
+
+  return "Failed"
+
+
 
 
 app.run(debug=True)
