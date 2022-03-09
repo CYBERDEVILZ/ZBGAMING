a29b12961963edcd3aa97430eafef2e0ef0c36bd
Pranav <pranavcosmos4@gmail.com>
2022-03-10 00:04:10 +0530

    email validation added

--- backend/venv/server_files/api.py
+++ backend/venv/server_files/api.py
@@ -3,12 +3,21 @@ from flask import request
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+import re
 
 # FIREBASE INIT
 cred = credentials.Certificate('zbgaming-v1-firebase-adminsdk-2ozhj-4f38e5fc3e.json')
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 app = Flask(__name__)
+
+# FUNCTIONS
+def validateEmail(email):
+  result = re.match("^[A-Za-z0-9\.]+@[A-Za-z0-9\.]+\.[A-Za-z0-9]+$", email)
+  if result:
+    return True
+  else:
+    return False
 
 
 @app.route("/")
@@ -22,13 +31,12 @@ def userSignup():
   email = request.args.get('email')
   imageurl = None
 
-  # perform form validation here......
-  # username shouldn't be null and email should comply with apt regex
-  if (username != None and email != None):
-    docs = db.collection("userinfo").where("email", "==", email).get()
-    if len(docs) == 0:
-      db.collection("userinfo").document().set({"username": username, "email": email, "imageurl": imageurl})
-      return "Success"
+  if (username != (None or "") and email != (None or "")):
+    if validateEmail(email):
+      docs = db.collection("userinfo").where("email", "==", email).get()
+      if len(docs) == 0:
+        db.collection("userinfo").document().set({"username": username, "email": email, "imageurl": imageurl})
+        return "Success"
 
   return "Failed"
 
@@ -43,13 +51,12 @@ def organizerSignup():
   amountGiven = 0
   rating = 0.0
 
-  # perform form validation here.........
-  # username shouldn't be null and email should comply with apt regex
-  if (username != None and email != None):
-    docs = db.collection("organizer").where("email", "==", email).get()
-    if len(docs) == 0:
-      db.collection("organizer").document().set({"username": username, "email": email, "imageurl": imageurl, "special": special, "amountGiven":amountGiven, "rating": rating})
-      return "Success"
+  if (username != (None or "") and email != (None or "")):
+    if validateEmail(email):
+      docs = db.collection("organizer").where("email", "==", email).get()
+      if len(docs) == 0:
+        db.collection("organizer").document().set({"username": username, "email": email, "imageurl": imageurl, "special": special, "amountGiven":amountGiven, "rating": rating})
+        return "Success"
   
   return "Failed"
 
@@ -60,8 +67,8 @@ def register():
   matchuid = request.args.get("matchuid") 
   useruid = request.args.get("useruid")
   
-  # perform some validation here.......
   # create a separate table of users who are registered for that match
+  # register only if both email and user is verified
 
   if (matchuid != None and useruid != None):
 
@@ -80,9 +87,9 @@ def register():
       uid = matchuid
       name = ref_obj["name"]
     except:
-      return "Failure, no such doc"
+      return "Failed: No such match"
 
-    # retrieve the total registered and update the value to one [USE TRANSACTION!]
+    # retrieve the total registered and update increase it by one [USE TRANSACTION!]
     transaction = db.transaction()
     @firestore.transactional
     def updateRegisteredTeams(transaction, ref):
@@ -107,173 +114,6 @@ def register():
       return "Failed"
 
   return "Failed"
-
-
-# REGISTER CSGO (DISABLE FOR NOW!!!!!!!!!!!!!!!!!!)
-@app.route("/api/register/csgo")
-def register():
-  matchuid = request.args.get("matchuid") 
-  useruid = request.args.get("useruid")
-  
-  # perform some validation here.......
-  # create a separate table of users who are registered for that match
-
-  if (matchuid != None and useruid != None):
-
-    # checking for already registered..
-    user = db.collection("userinfo").document(useruid).collection("registered").get()
-    user = [user.id for user in user]
-    if matchuid in user:
-      return "Failed: Already registered"
-
-
-    ref = db.collection("csgo").document(matchuid)
-    ref_obj = ref.get().to_dict()
-    try:
-      date = ref_obj["date"]
-      matchType = "csgo"
-      uid = matchuid
-      name = ref_obj["name"]
-    except:
-      return "Failure, no such doc"
-
-    # retrieve the total registered and update the value to one [USE TRANSACTION!]
-    transaction = db.transaction()
-    @firestore.transactional
-    def updateRegisteredTeams(transaction, ref):
-      snapshot = ref.get(transaction = transaction)
-      reg = snapshot.get("reg")
-      try:
-        if reg < 10:
-          transaction.update(ref, {"reg": reg + 1})
-          return True
-        else:
-          return False
-      except:
-        return False
-
-    
-    result = updateRegisteredTeams(transaction, ref)
-
-    if result:
-      db.collection("userinfo").document(useruid).collection("registered").document(matchuid).set({"date": date, "matchType": matchType, "name": name, "uid": uid})
-      return "Success"
-    else:
-      return "Failed"
-
-  return "Failed"
-
-
-# REGISTER VALORANT (DISABLE FOR NOW!!!!!!!!!!!!!!!!!!)
-@app.route("/api/register/valo")
-def register():
-  matchuid = request.args.get("matchuid") 
-  useruid = request.args.get("useruid")
-  
-  # perform some validation here.......
-  # create a separate table of users who are registered for that match
-
-  if (matchuid != None and useruid != None):
-
-    # checking for already registered..
-    user = db.collection("userinfo").document(useruid).collection("registered").get()
-    user = [user.id for user in user]
-    if matchuid in user:
-      return "Failed: Already registered"
-
-
-    ref = db.collection("valo").document(matchuid)
-    ref_obj = ref.get().to_dict()
-    try:
-      date = ref_obj["date"]
-      matchType = "valo"
-      uid = matchuid
-      name = ref_obj["name"]
-    except:
-      return "Failure, no such doc"
-
-    # retrieve the total registered and update the value to one [USE TRANSACTION!]
-    transaction = db.transaction()
-    @firestore.transactional
-    def updateRegisteredTeams(transaction, ref):
-      snapshot = ref.get(transaction = transaction)
-      reg = snapshot.get("reg")
-      try:
-        if reg < 10:
-          transaction.update(ref, {"reg": reg + 1})
-          return True
-        else:
-          return False
-      except:
-        return False
-
-    
-    result = updateRegisteredTeams(transaction, ref)
-
-    if result:
-      db.collection("userinfo").document(useruid).collection("registered").document(matchuid).set({"date": date, "matchType": matchType, "name": name, "uid": uid})
-      return "Success"
-    else:
-      return "Failed"
-
-  return "Failed"
-
-
-# REGISTER FREEFIRE (DISABLE FOR NOW!!!!!!!!!!!!!!!!!!)
-@app.route("/api/register/freefire")
-def register():
-  matchuid = request.args.get("matchuid") 
-  useruid = request.args.get("useruid")
-  
-  # perform some validation here.......
-  # create a separate table of users who are registered for that match
-
-  if (matchuid != None and useruid != None):
-
-    # checking for already registered..
-    user = db.collection("userinfo").document(useruid).collection("registered").get()
-    user = [user.id for user in user]
-    if matchuid in user:
-      return "Failed: Already registered"
-
-
-    ref = db.collection("freefire").document(matchuid)
-    ref_obj = ref.get().to_dict()
-    try:
-      date = ref_obj["date"]
-      matchType = "freefire"
-      uid = matchuid
-      name = ref_obj["name"]
-    except:
-      return "Failure, no such doc"
-
-    # retrieve the total registered and update the value to one [USE TRANSACTION!]
-    transaction = db.transaction()
-    @firestore.transactional
-    def updateRegisteredTeams(transaction, ref):
-      snapshot = ref.get(transaction = transaction)
-      reg = snapshot.get("reg")
-      try:
-        if reg < 50:
-          transaction.update(ref, {"reg": reg + 1})
-          return True
-        else:
-          return False
-      except:
-        return False
-
-    
-    result = updateRegisteredTeams(transaction, ref)
-
-    if result:
-      db.collection("userinfo").document(useruid).collection("registered").document(matchuid).set({"date": date, "matchType": matchType, "name": name, "uid": uid})
-      return "Success"
-    else:
-      return "Failed"
-
-  return "Failed"
-
-
 
 
 app.run(debug=True)
