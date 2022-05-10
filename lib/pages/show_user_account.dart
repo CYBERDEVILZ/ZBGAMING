08c32de6b222b16593340182e514d4b4c7982b99
CYBERDEVILZ <pranavcosmos4@gmail.com>
2022-05-11 02:55:49 +0530

    basic design added

--- lib/pages/show_user_account.dart
+++ lib/pages/show_user_account.dart
@@ -15,6 +15,67 @@ class ShowUserAccount extends StatelessWidget {
           ),
           const CircleAvatar(
             maxRadius: 70,
+          ),
+          Container(
+            margin: const EdgeInsets.all(20),
+            child: Card(
+              child: Container(
+                alignment: Alignment.center,
+                height: 500,
+                width: double.infinity,
+                margin: const EdgeInsets.only(
+                    top: 20, bottom: 10, left: 15, right: 15),
+                child: Column(
+                  children: [
+                    const Text("Name"),
+                    Container(
+                      margin: const EdgeInsets.only(
+                          top: 5, bottom: 20, left: 10, right: 10),
+                      padding: const EdgeInsets.all(10),
+                      alignment: Alignment.center,
+                      constraints: const BoxConstraints(minHeight: 45),
+                      decoration: const BoxDecoration(
+                          color: Color.fromARGB(255, 105, 164, 212)),
+                      width: double.infinity,
+                      child: const Text(
+                        "CYBERDEVILZ",
+                        style: TextStyle(fontSize: 20, color: Colors.white),
+                      ),
+                    ),
+                    const Text("Level"),
+                    Container(
+                      margin: const EdgeInsets.only(
+                          top: 5, bottom: 20, left: 10, right: 10),
+                      padding: const EdgeInsets.all(10),
+                      alignment: Alignment.center,
+                      constraints: const BoxConstraints(minHeight: 45),
+                      decoration: const BoxDecoration(
+                          color: Color.fromARGB(255, 70, 124, 168)),
+                      width: double.infinity,
+                      child: const Text(
+                        "VETERAN",
+                        style: TextStyle(fontSize: 20, color: Colors.white),
+                      ),
+                    ),
+                    const Text("Contact"),
+                    Container(
+                      margin: const EdgeInsets.only(
+                          top: 5, bottom: 20, left: 10, right: 10),
+                      padding: const EdgeInsets.all(10),
+                      alignment: Alignment.center,
+                      constraints: const BoxConstraints(minHeight: 45),
+                      decoration: const BoxDecoration(
+                          color: Color.fromARGB(255, 29, 68, 100)),
+                      width: double.infinity,
+                      child: const Text(
+                        "pranavcosmos4@gmail.com",
+                        style: TextStyle(fontSize: 20, color: Colors.white),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
           )
         ],
       )),
