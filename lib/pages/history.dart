85c50bf8a9d9b4dac0e7b65ba895c6956324d1bc
CYBERDEVILZ <pranavcosmos4@gmail.com>
2022-07-01 13:09:38 +0530

    minor change

--- lib/pages/history.dart
+++ lib/pages/history.dart
@@ -11,32 +11,27 @@ class History extends StatefulWidget {
 }
 
 class _HistoryState extends State<History> {
-  Stream<QuerySnapshot<Map<String, dynamic>>> collectionStream =
-      FirebaseFirestore.instance
-          .collection('userinfo')
-          .doc(FirebaseAuth.instance.currentUser?.uid)
-          .collection('history')
-          .snapshots();
+  Stream<QuerySnapshot<Map<String, dynamic>>> collectionStream = FirebaseFirestore.instance
+      .collection('userinfo')
+      .doc(FirebaseAuth.instance.currentUser?.uid)
+      .collection('history')
+      .snapshots();
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       top: true,
       child: Scaffold(
-          appBar: AppBar(
-              title: const Text("History"), centerTitle: true, elevation: 0),
+          appBar: AppBar(title: const Text("History"), centerTitle: true, elevation: 0),
           body: StreamBuilder(
               stream: collectionStream,
-              builder: (BuildContext context,
-                  AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+              builder: (BuildContext context, AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
                 if (snapshot.hasError) {
                   return const Center(child: Text('Something went wrong'));
                 }
 
                 if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const Center(
-                      child:
-                          CircularProgressIndicator(color: Colors.lightBlue));
+                  return const Center(child: CircularProgressIndicator(color: Colors.lightBlue));
                 }
 
                 return ListView(
@@ -46,63 +41,59 @@ class _HistoryState extends State<History> {
                               ListTile(
                                 leading: ClipRRect(
                                     child: Image.asset(
-                                      "assets/images/" +
-                                          e["matchType"] +
-                                          ".jpg",
+                                      "assets/images/" + e["matchType"] + ".jpg",
                                       width: 100,
                                     ),
                                     borderRadius: BorderRadius.circular(10)),
                                 title: Text(
                                   e["name"],
-                                  style: const TextStyle(
-                                      fontWeight: FontWeight.bold),
+                                  style: const TextStyle(fontWeight: FontWeight.bold),
                                 ),
-                                subtitle: Text(DateToString()
-                                    .dateToString(e["date"].toDate())),
-                                trailing: e["won"] == 0
+                                subtitle: Text(DateToString().dateToString(e["date"].toDate())),
+                                trailing: e["won"] == -1
                                     ? Container(
                                         decoration: BoxDecoration(
-                                          borderRadius:
-                                              BorderRadius.circular(10),
-                                          color: Colors.red.withOpacity(0.3),
+                                          borderRadius: BorderRadius.circular(10),
+                                          color: Colors.blue.withOpacity(0.3),
                                         ),
                                         padding: const EdgeInsets.all(8),
-                                        child: const Text("Lost"),
+                                        child: const Text("Registered"),
                                       )
-                                    : e["won"] == 1
+                                    : e["won"] == 0
                                         ? Container(
                                             decoration: BoxDecoration(
-                                              borderRadius:
-                                                  BorderRadius.circular(10),
-                                              color:
-                                                  Colors.green.withOpacity(0.3),
+                                              borderRadius: BorderRadius.circular(10),
+                                              color: Colors.red.withOpacity(0.3),
                                             ),
                                             padding: const EdgeInsets.all(8),
-                                            child: const Text("Won"),
+                                            child: const Text("Lost"),
                                           )
-                                        : e["won"] == 2
+                                        : e["won"] == 1
                                             ? Container(
                                                 decoration: BoxDecoration(
-                                                  borderRadius:
-                                                      BorderRadius.circular(10),
-                                                  color: Colors.yellow
-                                                      .withOpacity(0.3),
+                                                  borderRadius: BorderRadius.circular(10),
+                                                  color: Colors.green.withOpacity(0.3),
                                                 ),
-                                                padding:
-                                                    const EdgeInsets.all(8),
-                                                child: const Text("Cancelled"),
+                                                padding: const EdgeInsets.all(8),
+                                                child: const Text("Won"),
                                               )
-                                            : Container(
-                                                decoration: BoxDecoration(
-                                                  borderRadius:
-                                                      BorderRadius.circular(10),
-                                                  color: Colors.red
-                                                      .withOpacity(0.3),
-                                                ),
-                                                padding:
-                                                    const EdgeInsets.all(8),
-                                                child: const Text("Error"),
-                                              ),
+                                            : e["won"] == 2
+                                                ? Container(
+                                                    decoration: BoxDecoration(
+                                                      borderRadius: BorderRadius.circular(10),
+                                                      color: Colors.yellow.withOpacity(0.3),
+                                                    ),
+                                                    padding: const EdgeInsets.all(8),
+                                                    child: const Text("Cancelled"),
+                                                  )
+                                                : Container(
+                                                    decoration: BoxDecoration(
+                                                      borderRadius: BorderRadius.circular(10),
+                                                      color: Colors.red.withOpacity(0.3),
+                                                    ),
+                                                    padding: const EdgeInsets.all(8),
+                                                    child: const Text("Error"),
+                                                  ),
                               ),
                               const Divider(
                                 height: 0,
