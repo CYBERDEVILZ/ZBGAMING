993f1a5cf7571a889ee0502e7c8f053252d40699
CYBERDEVILZ <pranavcosmos4@gmail.com>
2022-10-27 16:25:44 +0530

    api endpoint list added

---
+++ lib/utils/apistring.dart
@@ -0,0 +1,20 @@
+class ApiEndpoints {
+  static String baseUrl = "https://443f-117-207-156-72.in.ngrok.io";
+  static String baseLiveUrl = "https://zbgaming-api-f77x3y5ota-uc.a.run.app/";
+  static String verifierSignup = "/api/verifier/thiswillmakefindingthisapidifficult/signup";
+  static String userSignup = "/api/user_signup";
+  static String organizerSignup = "/api/organizer_signup";
+  static String createMatch = "/api/create";
+  static String userLevelCalculate = "/api/userlevelcalculate";
+  static String userLevelCalculateAlternative = "/api/userlevelcalculatealternative";
+  static String register = "/api/register";
+  static String paidOrder = "/api/paidOrder";
+  static String startMatch = "/api/startMatch";
+  static String stopMatch = "/api/stopMatch";
+  static String reportMatch = "/api/reportMatch";
+  static String rate = "/api/rate";
+  static String cancel = "/api/cancelTheMatch";
+  static String buyCoins = "/api/buyCoins";
+  static String verifyOrderForCoins = "/api/verifyBuyingCoins";
+  static String selectWinner = "/api/selectWinner";
+}
